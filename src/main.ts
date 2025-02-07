2dae6c9de6ce08d49bced31f75ca7758e61c8eb0
Pablo Miralles <pablo@mowomo.com>
2025-02-07 16:53:45 +0100

    codigo ajustado con tras los comentarios de Antonio

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,63 @@
 import "./style.css";
 
+let turn = 0;
 const counter = document.querySelector(".counter-box__number");
-let counterNumber = Number(
-	document.querySelector(".counter-box__number")?.textContent
-);
+
 const nextBtn = document.querySelector(".counter-box__btn.is-next");
 const prevBtn = document.querySelector(".counter-box__btn.is-prev");
 const resetBtn = document.querySelector(".counter-box__btn.is-reset");
+
+const customInput = document.querySelector(".counter-box__input");
 const customBtn = document.querySelector(".counter-box__btn.is-custom");
 
 const formatNumber = (element: number) => {
 	if (counter) {
 		counter.textContent = element.toString().padStart(2, "0");
-		counterNumber = Number(counter.textContent);
 	}
 };
 
 const setCustomNumber = () => {
-	let customNumber = Number(prompt("Select custom number"));
-
-	if (customNumber !== undefined && customNumber !== null) {
-		while (
-			typeof customNumber === "number" &&
-			(Number.isNaN(customNumber) || customNumber <= 0)
+	if (customInput && customInput instanceof HTMLInputElement) {
+		let customInputValue = Number(customInput.value);
+		if (
+			customInputValue !== undefined &&
+			customInputValue !== null &&
+			customInputValue >= 0
 		) {
-			alert("It needs to be a positive number");
-			customNumber = Number(prompt("Select custom number"));
+			turn = customInputValue;
+			formatNumber(turn);
+		} else {
+			alert(
+				"Escribe un número válido por favor. Tiene que ser 0 o positivo."
+			);
 		}
-
-		formatNumber(customNumber);
 	}
 };
 
-nextBtn?.addEventListener("click", () => {
-	formatNumber(++counterNumber);
-});
-prevBtn?.addEventListener("click", () => {
-	if (counterNumber > 0) {
-		formatNumber(--counterNumber);
-	}
-});
-resetBtn?.addEventListener("click", () => {
-	formatNumber((counterNumber = 0));
-});
-customBtn?.addEventListener("click", setCustomNumber);
+if (nextBtn && nextBtn instanceof HTMLButtonElement) {
+	nextBtn?.addEventListener("click", () => {
+		formatNumber(++turn);
+	});
+}
+
+if (prevBtn && prevBtn instanceof HTMLButtonElement) {
+	prevBtn?.addEventListener("click", () => {
+		if (turn > 0) {
+			formatNumber(--turn);
+		}
+	});
+}
+
+if (resetBtn && resetBtn instanceof HTMLButtonElement) {
+	resetBtn.addEventListener("click", () => {
+		formatNumber((turn = 0));
+	});
+}
+
+if (customBtn && customBtn instanceof HTMLButtonElement) {
+	customBtn.addEventListener("click", setCustomNumber);
+}
+
+// Para que se ejecute una primera vez, ya que el número del div
+// ya no está puesto en la estructura HTML, si no que lo indico yo en el JS
+formatNumber(turn);
